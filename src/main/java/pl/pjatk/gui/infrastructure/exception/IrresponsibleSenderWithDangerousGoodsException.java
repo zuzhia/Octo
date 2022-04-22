8142879fd5fea9ae1057752bc0de80a45cf434f7
ejdam0 <adam.strzelecki0@gmail.com>
2022-04-22 22:55:08 +0200

    Add exception

--- src/main/java/pl/pjatk/gui/infrastructure/exception/IrresponsibleSenderWithDangerousGoodsException.java
+++ src/main/java/pl/pjatk/gui/infrastructure/exception/IrresponsibleSenderWithDangerousGoodsException.java
@@ -1,4 +1,9 @@
 package pl.pjatk.gui.infrastructure.exception;
 
-public class IrresponsibleSenderWithDangerousGoodsException {
+import java.time.LocalDate;
+
+public class IrresponsibleSenderWithDangerousGoodsException extends RuntimeException {
+    public IrresponsibleSenderWithDangerousGoodsException(LocalDate arrivalDate, LocalDate disposalDate, int id) {
+        super("Container: " + id + " (arrival date: " + arrivalDate + ") was disposed on " + disposalDate);
+    }
 }
