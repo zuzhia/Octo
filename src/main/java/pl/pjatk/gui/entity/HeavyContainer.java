0454667fe923ca30a61db007fb62215d664d43ab
Zuzhia <104226942+zuzhia@users.noreply.github.com>
2022-04-22 23:08:25 +0200

    Update HeavyContainer.java

--- src/main/java/pl/pjatk/gui/entity/HeavyContainer.java
+++ src/main/java/pl/pjatk/gui/entity/HeavyContainer.java
@@ -2,4 +2,10 @@ package pl.pjatk.gui.entity;
 
 public class HeavyContainer extends BasicContainer{
 
+    private int id;
+    private int weightNetto;
+    private int weightBrutto;
+    private int volume;
+    private String sender;
+    private String color;
 }
