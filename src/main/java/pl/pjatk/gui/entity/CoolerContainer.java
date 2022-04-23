85116e8004cec9e7fd6259230c302778321cdd24
Zuzhia <104226942+zuzhia@users.noreply.github.com>
2022-04-23 08:31:41 +0200

    Update CoolerContainer.java

--- src/main/java/pl/pjatk/gui/entity/CoolerContainer.java
+++ src/main/java/pl/pjatk/gui/entity/CoolerContainer.java
@@ -2,12 +2,6 @@ package pl.pjatk.gui.entity;
 
 public class CoolerContainer extends BasicContainer{
   
-    private int id;
-    private double weightNetto;
-    private double weightBrutto;
-    private double weightTara;
-    private int volume;
     private double temperatureInsideContainer;
-    private String sender;
-    private String color;
+    
 }
