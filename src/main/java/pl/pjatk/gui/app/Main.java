62c9a79f87c4a75f7d295119ea81ccfc9c978923
Zuzhia <104226942+zuzhia@users.noreply.github.com>
2022-04-23 19:27:58 +0200

    Update Main.java

--- src/main/java/pl/pjatk/gui/app/Main.java
+++ src/main/java/pl/pjatk/gui/app/Main.java
@@ -3,7 +3,8 @@ package pl.pjatk.gui.app;
 public class Main{
     public static void main(String[] args){
 
-
+        Harbour harbour = new Harbour();
+        Warehouse warehouse = new Warehoue();
 
 
     }
